e396abf93022c609703476f02c2aa43ee7f810a1
EKbfh <earthquake_90@mail.ru>
2019-10-21 16:30:28 +0300

    Change create empty dirs to script

--- pre.sh
+++ pre.sh
@@ -6,6 +6,24 @@
 INSTALLPATH=/opt/noc-dc
 TMPPATH=/tmp/$(date +%s)
 
+function CREATEDIR {
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/datasources
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/notifiers
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/dashboards
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/dashboards
+    mkdir -p $INSTALLPATH/data/promgrafana/plugins
+    mkdir -p $INSTALLPATH/data/consul
+    mkdir -p $INSTALLPATH/data/nsq
+    mkdir -p $INSTALLPATH/data/promvm
+    mkdir -p $INSTALLPATH/data/prometheus/metrics
+    mkdir -p $INSTALLPATH/data/mongo
+    mkdir -p $INSTALLPATH/data/postgres
+    mkdir -p $INSTALLPATH/data/nginx/ssl
+    mkdir -p $INSTALLPATH/data/clickhouse/data
+    mkdir -p $INSTALLPATH/data/grafana/plugins
+
+
+}
 
 function SETPERMISSION {
     chmod 777 -R $INSTALLPATH/data/clickhouse/data
@@ -26,13 +44,16 @@ if [ -n "$1" ]
     then
         if [ "$1" = "all" ]
             then
+		CREATEDIR
                 SETPERMISSION
                 SETUPPROMGRAFANA
         elif [ "$1" = "perm" ]
             then
+		CREATEDIR
                 SETPERMISSION
         elif [ "$1" = "grafana" ]
             then
+		CREATEDIR
                 SETUPPROMGRAFANA
         else
             echo "Unknown parameter.  Use: all, perm, grafana"
