ee29677b6e0591205033ae85915aec4c32ecb416
ezombie <lozovoy@satel.org>
2019-10-21 21:58:06 +0100

    Pre.sh

---
+++ pre.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# setup permissions
+# setup promgrafana dashboards\sources
+
+INSTALLPATH=/opt/noc-dc
+TMPPATH=$(mktemp -d -p /tmp)
+TMPPATH1=$(mktemp -d -p /tmp)
+
+function CREATEDIR {
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/datasources
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/notifiers
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/provisioning/dashboards
+    mkdir -p $INSTALLPATH/data/promgrafana/etc/dashboards
+    mkdir -p $INSTALLPATH/data/promgrafana/plugins
+    mkdir -p $INSTALLPATH/data/promgrafana/db
+    mkdir -p $INSTALLPATH/data/promvm
+    mkdir -p $INSTALLPATH/data/prometheus/metrics
+    mkdir -p $INSTALLPATH/data/prometheus/etc/rules.d
+    mkdir -p $INSTALLPATH/data/consul
+    mkdir -p $INSTALLPATH/data/clickhouse/data
+    mkdir -p $INSTALLPATH/data/nsq
+    mkdir -p $INSTALLPATH/data/mongo
+    mkdir -p $INSTALLPATH/data/postgres
+    mkdir -p $INSTALLPATH/data/nginx/ssl
+    mkdir -p $INSTALLPATH/data/grafana/plugins
+    mkdir -p $INSTALLPATH/data/sentry/redis
+    mkdir -p $INSTALLPATH/data/sentry/pg
+}
+
+function SETPERMISSION {
+    chown 101 -R $INSTALLPATH/data/clickhouse/data
+    chown 999 -R $INSTALLPATH/data/postgres
+    chown 999 -R $INSTALLPATH/data/mongo
+    chown 472 -R $INSTALLPATH/data/grafana/
+    chown 65534 -R $INSTALLPATH/data/prometheus/metrics
+    chown 472 -R $INSTALLPATH/data/promgrafana/plugins
+    chown 999 -R $INSTALLPATH/data/sentry/redis
+}
+
+function SETUPPROMGRAFANA {
+    cd "$TMPPATH" && git clone https://code.getnoc.com/e_zombie/grafana-dashboard-import.git .
+    cp -f -r "$TMPPATH"/dashboards/* "$INSTALLPATH"/data/promgrafana/etc/dashboards
+    cp -f -r "$TMPPATH"/provisioning/* "$INSTALLPATH"/data/promgrafana/etc/provisioning
+}
+
+function SETUPPROMRULES {
+   cd "$TMPPATH1" && git clone https://code.getnoc.com/noc/noc-prometheus-alerts.git .
+   cp -f "$TMPPATH1"/*.yml "$INSTALLPATH"/data/prometheus/etc/rules.d
+}
+
+
+if [ -n "$1" ]
+    then
+        if [ "$1" = "all" ]
+            then
+		        CREATEDIR
+                SETPERMISSION
+                SETUPPROMGRAFANA
+                SETUPPROMRULES
+        elif [ "$1" = "perm" ]
+            then
+		        CREATEDIR
+                SETPERMISSION
+        elif [ "$1" = "grafana" ]
+            then
+		        CREATEDIR
+                SETUPPROMGRAFANA
+        elif [ "$1" = "promrules" ]
+            then
+                CREATEDIR
+                SETUPPROMRULES
+        else
+            echo "Unknown parameter.  Use: all, perm, grafana, promrules"
+        fi
+else
+    echo "No  parameters found. Use: all, perm, grafana, promrules" 
+fi
