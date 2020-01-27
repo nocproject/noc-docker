be73fcd78772d182640d2ab8da5f2c3421acaadb
e_zombie <beleberda_9>
2020-01-27 19:44:42 +0400

    * fix sentry.env generation * fix docker-compose-infra.yml syntax

--- pre.sh
+++ pre.sh
@@ -56,17 +56,23 @@ function SETUPPROMRULES {
 function SETUPSENTRY() {
     if [ ! -f $INSTALLPATH/data/sentry/sentry.env ]
         then
+            GENERATE_PASSWORD="$(date -d "Oct 1 2000" +%s)"
+
             echo "Setup Sentry env in $INSTALLPATH/data/sentry/sentry.env"
-            { echo SENTRY_SECRET_KEY="$(date -d "Oct 22 1974" +%s)"
-              echo SENTRY_DB_PASSWORD="$(date -d "now" +%s)"
-              echo SENTRY_POSTGRES_HOST=sentry-postgres
+            echo "after firsh start need run command for for run migration and setup admin user\passwd"
+            echo "docker exec -ti noc-dc_sentry_1 sentry upgrade"
+            { echo SENTRY_POSTGRES_HOST=sentry-postgres
+              echo SENTRY_DB_NAME=sentry
               echo SENTRY_DB_USER=sentry
+              echo SENTRY_DB_PASSWORD=$GENERATE_PASSWORD
+              echo SENTRY_SECRET_KEY="$(date -d "Oct 22 1974" +%s)"
               echo SENTRY_REDIS_HOST=sentry-redis
               echo SENTRY_METRICS_SAMPLE_RATE=0.9
               echo POSTGRES_USER=sentry
               echo POSTGRES_DBNAME=sentry
-              echo POSTGRES_PASSWORD="$(date -d "Oct 1 2000" +%s)"
-              echo POSTGRES_DBPASS="$(date -d "Oct 14 1199" +%s)"
+              echo POSTGRES_PASSWORD=$GENERATE_PASSWORD
+              echo "Important!!! POSTGRES_PASSWORD == SENTRY_DB_PASSWORD"
+#              echo POSTGRES_DBPASS="$(date -d "Oct 14 1199" +%s)"
             } >> $INSTALLPATH/data/sentry/sentry.env
     fi
 }
