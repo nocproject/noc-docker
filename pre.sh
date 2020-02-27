cb9f6b2f302ea39e8b45a1720ab61553d142558a
e_zombie <beleberda_9>
2020-02-27 22:56:34 +0400

    Make echo more readable

--- pre.sh
+++ pre.sh
@@ -40,15 +40,17 @@ SETPERMISSION() {
 }
 
 SETUPPROMGRAFANA() {
-    echo "Clone GRAFANA dashboards from code.getnoc.com"
-    cd "$TMPPATH" && git clone https://code.getnoc.com/noc/grafana-selfmon-dashboards.git .
+    echo "Setup GRAFANA dashboards from code.getnoc.com"
+    echo "---"
+    cd "$TMPPATH" && git clone -q https://code.getnoc.com/noc/grafana-selfmon-dashboards.git .
     cp -f -r "$TMPPATH"/dashboards/* "$INSTALLPATH"/data/promgrafana/etc/dashboards
     cp -f -r "$TMPPATH"/provisioning/* "$INSTALLPATH"/data/promgrafana/etc/provisioning
 }
 
 SETUPPROMRULES() {
-    echo "Clone PROMETHEUS alert rules from code.getnoc.com"
-    cd "$TMPPATH1" && git clone https://code.getnoc.com/noc/noc-prometheus-alerts.git .
+    echo "Setup PROMETHEUS alert rules from code.getnoc.com"
+    echo "---"
+    cd "$TMPPATH1" && git clone -q https://code.getnoc.com/noc/noc-prometheus-alerts.git .
     cp -f "$TMPPATH1"/*.yml "$INSTALLPATH"/data/prometheus/etc/rules.d
 }
 
@@ -59,8 +61,9 @@ SETUPSENTRY() {
             GENERATE_PASSWORD="$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
 
             echo "Setup Sentry env in $INSTALLPATH/data/sentry/sentry.env"
-            echo "after firsh start need run command for for run migration and setup admin user\passwd"
+            echo "after firsh start container need run command for make migration and setup admin user\passwd"
             echo "cd $INSTALLPATH && docker-compose exec sentry sentry upgrade"
+            echo "---"
             { echo SENTRY_POSTGRES_HOST=sentry-postgres
               echo SENTRY_DB_NAME=sentry
               echo SENTRY_DB_USER=sentry
@@ -80,6 +83,7 @@ SETUPNOCCONF() {
     if [ ! -f "$INSTALLPATH"/data/noc/etc/noc.conf ]
         then
             echo "Copy " "$INSTALLPATH"/data/noc/etc/noc.conf.example " to " "$INSTALLPATH"/data/noc/etc/noc.conf
+            echo "---"
             # shellcheck disable=SC2086
             cp $INSTALLPATH/data/noc/etc/noc.conf.example $INSTALLPATH/data/noc/etc/noc.conf
     fi
@@ -91,6 +95,7 @@ SETUPENV() {
     if [ ! -f "$INSTALLPATH"/.env ]
         then
             echo "Setup COMPOSEPATH=$INSTALLPATH in $INSTALLPATH/.env"
+            echo "---"
             { echo "COMPOSEPATH=$INSTALLPATH"
               echo "COMPOSETAG=$PARAM_TAG"
             } >> "$INSTALLPATH"/.env
@@ -101,13 +106,13 @@ while [ -n "$1" ]
 do
     case "$1" in
         -t) PARAM_TAG="$2"
-            echo "Found the -t option, with parameter value $PARAM_TAG"
+            #echo "Found the -t option, with parameter value $PARAM_TAG"
             shift ;;
         -p) PARAM_P="$2"
-            echo "Found the -p option, with parameter value $PARAM_P"
+            #echo "Found the -p option, with parameter value $PARAM_P"
             shift ;;
         -d) INSTALLPATH="$2"
-            echo "Found the -d option, with parameter value $INSTALLPATH"
+            #echo "Found the -d option, with parameter value $INSTALLPATH"
             shift ;;
         -h) echo "Example: ./pre.sh -p all -d /opt/noc-dc -t stable"
             shift ;;
@@ -121,14 +126,16 @@ done
 if [ -z "$INSTALLPATH" ]
     then
         INSTALLPATH=/opt/noc-dc
-        echo "Setup NOC-DC to $INSTALLPATH"
+        echo "Setup NOC-DC to: $INSTALLPATH"
+        echo "---"
 fi
 
 if [ -z "$PARAM_TAG" ]
     then
         PARAM_TAG="stable"
-        echo "Use image  noc/noc/code:$PARAM_TAG"
-        echo "All tags in https://code.getnoc.com/noc/noc/container_registry"
+        echo "Setup Docker use image with tag: $PARAM_TAG"
+        echo "See all tags in https://code.getnoc.com/noc/noc/container_registry"
+        echo "---"
 fi
 
 if [ -n "$PARAM_P" ]
