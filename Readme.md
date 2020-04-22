941bbcc0239b3971cdcf90e83e1216366679853a
ezombie <lozovoy@satel.org>
2020-04-22 11:29:14 +0100

    Add proxy support

--- Readme.md
+++ Readme.md
@@ -1,6 +1,48 @@
 NOC Project installation via docker
 ==================================
 
+# NOC Project
+![logo_180px](https://cdn.getnoc.com/logo/logo_180px.png)
+
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)](https://docs.getnoc.com)
+[![License: BSD 3-Clause License](https://img.shields.io/badge/License-BSD-brightgreen.svg)](https://choosealicense.com/licenses/bsd-3-clause/)
+
+
+> NOC project is an Operation Support System (OSS) for telecom companies, service providers, and enterprise Network Operation Centers (NOC)
+
+### [Homepage](https://getnoc.com/)
+
+Features
+----
++ Fault Management
+  + Root Cause Analysis, topology correlation, escalation.
+    Active probing and passive alarm condition detection
+    in syslog and SNMP traps.
++ Performance Management
+  + Flexible metrics collection via SNMP and CLI. Long-term metrics storage.
+     Automatic configuration of dashboards. 
+     Complex threshold control with window functions.
++ Inventory
+  + Centralized database of physical and logical resources.
+    Tracks physical assets like chassis and modules.
+    Tracks logical resources (IP, VLAN, Phone Numbers) usage as well.
+    IP address planning via IPAM.
++ Discovery
+  + Sophisticated multi-protocol network topology discovery. 
+    Configuration and resource usage discovery.
++ Vendor-agnostic
+  + Breaking the vendor locks with 80+ of supported vendors.
+    Adding new vendors and platforms on daily routine basis.
++ LargeScale
+  + Starting from simple single-node installation and up to clusters controlling
+    world’s largest networks with 300k+ of objects.
++ Integration
+  + ETL interface allows to import data from existing systems.
+    DataStream API and NBI interfaces provide services to other system.
++ Big Data
+  + Introduces Big Data analysis to the Network Management. 
+    Builtin analytics database and provided BI tools allows to access magic
+
 Install
 -------
 Disable SELINUX. See distro docs.
@@ -185,3 +227,37 @@ A: You need run
    Setup admin user and password.
 
    Go to https://0.0.0.0:9000 to login in Sentry
+
+Q: I connect to the Internet through a proxy server.
+   How do I configure the installation of all system components.
+
+A: You need to set the environment variable 
+   HTTPS_PROXY (necessarily in UPPER CASE) 
+   and the script `./pre.sh` uses this variable to configure containers.
+   You can check the current settings by running the command
+   ```shell script
+   env | grep -i proxy
+    HTTPS_PROXY=http://<proxyIP>:<proxyPORT>
+   ```
+   If your proxy server address has changed - edit the file `.env.proxy`
+
+Contributing
+----
+Contributions, issues and feature requests are welcome!
+
+Feel free to check 
+[issues page](https://code.getnoc.com/noc/noc/issues/).
+
+Feel free to check Docker specific 
+[issues page](https://code.getnoc.com/noc/noc-dc/issues/).
+
+Contact us:
+----
+* Telegram group:  https://t.me/nocproject
+* Official site: https://getnoc.com
+
+License
+----
+This project is
+[BSD 3-Clause License](https://choosealicense.com/licenses/bsd-3-clause/) 
+licensed.
--- pre.sh
+++ pre.sh
@@ -4,154 +4,183 @@ TMPPATH=$(mktemp -d -p /tmp)
 TMPPATH1=$(mktemp -d -p /tmp)
 TMPPATH2=$(mktemp -d -p /tmp)
 
+CHECKWAN() {
+  echo "Check internet connection"
+  echo "----"
+  touch "$INSTALLPATH"/.env.proxy
+  ping -c 1 -q google.com > /dev/null 2>&1
+  if [ $? ]
+    then
+      PROXYFORWAN="$HTTPS_PROXY"
+      if [ -z "$PROXYFORWAN" ]
+        then
+          echo "You need setup 'HTTPS_PROXY' parameter"
+          echo "Example: export HTTPS_PROXY=http://<ip>:<port>"
+          exit
+        else
+          echo "Detected proxy:" "$PROXYFORWAN"
+          echo "Create .env.proxy file"
+          echo "----"
+          {
+            echo "https_proxy=$PROXYFORWAN"
+          } > "$INSTALLPATH"/.env.proxy
+      fi
+    else
+      echo "Proxy not detected."
+      echo "If you have a proxy - configure HTTPS_PROXY parameters"
+      echo "Example: export HTTPS_PROXY=http://<ip>:<port>"
+      echo "----"
+
+  fi
+}
 CREATEDIR() {
-    mkdir -p "$INSTALLPATH"/data/clickhouse/data
-    mkdir -p "$INSTALLPATH"/data/consul
-    mkdir -p "$INSTALLPATH"/data/grafana/plugins
-    mkdir -p "$INSTALLPATH"/data/mongo
-    mkdir -p "$INSTALLPATH"/data/mongorestore
-    mkdir -p "$INSTALLPATH"/data/nginx/ssl
-    mkdir -p "$INSTALLPATH"/data/noc/beef
-    mkdir -p "$INSTALLPATH"/data/noc/code
-    mkdir -p "$INSTALLPATH"/data/noc/custom
-    mkdir -p "$INSTALLPATH"/data/noc/etc
-    mkdir -p "$INSTALLPATH"/data/nsq
-    mkdir -p "$INSTALLPATH"/data/postgres
-    mkdir -p "$INSTALLPATH"/data/postgresrestore
-    mkdir -p "$INSTALLPATH"/data/prometheus/etc/rules.d
-    mkdir -p "$INSTALLPATH"/data/prometheus/etc/rules.custom.d
-    mkdir -p "$INSTALLPATH"/data/prometheus/metrics
-    mkdir -p "$INSTALLPATH"/data/promgrafana/db
-    mkdir -p "$INSTALLPATH"/data/promgrafana/etc/dashboards
-    mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/dashboards
-    mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/datasources
-    mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/notifiers
-    mkdir -p "$INSTALLPATH"/data/promgrafana/plugins
-    mkdir -p "$INSTALLPATH"/data/promvm
-    mkdir -p "$INSTALLPATH"/data/sentry/pg
-    mkdir -p "$INSTALLPATH"/data/sentry/redis
+  mkdir -p "$INSTALLPATH"/data/clickhouse/data
+  mkdir -p "$INSTALLPATH"/data/consul
+  mkdir -p "$INSTALLPATH"/data/grafana/plugins
+  mkdir -p "$INSTALLPATH"/data/mongo
+  mkdir -p "$INSTALLPATH"/data/mongorestore
+  mkdir -p "$INSTALLPATH"/data/nginx/ssl
+  mkdir -p "$INSTALLPATH"/data/noc/beef
+  mkdir -p "$INSTALLPATH"/data/noc/code
+  mkdir -p "$INSTALLPATH"/data/noc/custom
+  mkdir -p "$INSTALLPATH"/data/noc/etc
+  mkdir -p "$INSTALLPATH"/data/nsq
+  mkdir -p "$INSTALLPATH"/data/postgres
+  mkdir -p "$INSTALLPATH"/data/postgresrestore
+  mkdir -p "$INSTALLPATH"/data/prometheus/etc/rules.d
+  mkdir -p "$INSTALLPATH"/data/prometheus/etc/rules.custom.d
+  mkdir -p "$INSTALLPATH"/data/prometheus/metrics
+  mkdir -p "$INSTALLPATH"/data/promgrafana/db
+  mkdir -p "$INSTALLPATH"/data/promgrafana/etc/dashboards
+  mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/dashboards
+  mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/datasources
+  mkdir -p "$INSTALLPATH"/data/promgrafana/etc/provisioning/notifiers
+  mkdir -p "$INSTALLPATH"/data/promgrafana/plugins
+  mkdir -p "$INSTALLPATH"/data/promvm
+  mkdir -p "$INSTALLPATH"/data/sentry/pg
+  mkdir -p "$INSTALLPATH"/data/sentry/redis
 }
 
 SETPERMISSION() {
-    chown 101   -R "$INSTALLPATH"/data/clickhouse/data
-    chown 472   -R "$INSTALLPATH"/data/grafana
-    chown 472   -R "$INSTALLPATH"/data/promgrafana
-    chown 65534 -R "$INSTALLPATH"/data/prometheus/metrics
-    chown 70    -R "$INSTALLPATH"/data/sentry/pg
-    chown 999   -R "$INSTALLPATH"/data/mongo
-    chown 999   -R "$INSTALLPATH"/data/postgres
-    chown 999   -R "$INSTALLPATH"/data/sentry/redis
+  chown 101   -R "$INSTALLPATH"/data/clickhouse/data
+  chown 472   -R "$INSTALLPATH"/data/grafana
+  chown 472   -R "$INSTALLPATH"/data/promgrafana
+  chown 65534 -R "$INSTALLPATH"/data/prometheus/metrics
+  chown 70    -R "$INSTALLPATH"/data/sentry/pg
+  chown 999   -R "$INSTALLPATH"/data/mongo
+  chown 999   -R "$INSTALLPATH"/data/postgres
+  chown 999   -R "$INSTALLPATH"/data/sentry/redis
 }
 
 SETUPPROMGRAFANA() {
-    echo "GRAFANA dashboards download from code.getnoc.com/noc/grafana-selfmon-dashboards"
-    echo "---"
-    cd "$TMPPATH" && git clone -q https://code.getnoc.com/noc/grafana-selfmon-dashboards.git .
-    cp -f -r "$TMPPATH"/dashboards/* "$INSTALLPATH"/data/promgrafana/etc/dashboards
-    cp -f -r "$TMPPATH"/provisioning/* "$INSTALLPATH"/data/promgrafana/etc/provisioning
+  echo "GRAFANA dashboards download from code.getnoc.com/noc/grafana-selfmon-dashboards"
+  echo "---"
+  cd "$TMPPATH" && git clone -q https://code.getnoc.com/noc/grafana-selfmon-dashboards.git .
+  cp -f -r "$TMPPATH"/dashboards/* "$INSTALLPATH"/data/promgrafana/etc/dashboards
+  cp -f -r "$TMPPATH"/provisioning/* "$INSTALLPATH"/data/promgrafana/etc/provisioning
 }
 
 SETUPPROMRULES() {
-    echo "PROMETHEUS alert rules download from code.getnoc.com/noc/noc-prometheus-alerts.git"
-    echo "---"
-    cd "$TMPPATH1" && git clone -q https://code.getnoc.com/noc/noc-prometheus-alerts.git .
-    cp -f "$TMPPATH1"/*.yml "$INSTALLPATH"/data/prometheus/etc/rules.d
+  echo "PROMETHEUS alert rules download from code.getnoc.com/noc/noc-prometheus-alerts.git"
+  echo "---"
+  cd "$TMPPATH1" && git clone -q https://code.getnoc.com/noc/noc-prometheus-alerts.git .
+  cp -f "$TMPPATH1"/*.yml "$INSTALLPATH"/data/prometheus/etc/rules.d
 }
 
 SETUPSENTRY() {
-    if [ ! -f "$INSTALLPATH"/data/sentry/sentry.env ]
-        then
+  if [ ! -f "$INSTALLPATH"/data/sentry/sentry.env ]
+    then
 # @TODO
-            GENERATE_PASSWORD="$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
+      GENERATE_PASSWORD="$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
 
-            echo "Sentry env write in $INSTALLPATH/data/sentry/sentry.env"
-            echo "after first start container need run command to make migration by setting up admin user passwd"
-            echo "cd $INSTALLPATH && docker-compose -f docker-compose-infra.yml exec sentry sentry upgrade"
-            echo "---"
-            { echo SENTRY_POSTGRES_HOST=sentry-postgres
-              echo SENTRY_DB_NAME=sentry
-              echo SENTRY_DB_USER=sentry
-              echo SENTRY_DB_PASSWORD="$GENERATE_PASSWORD"
-              echo SENTRY_SECRET_KEY="$(dd 'if=/dev/random' 'bs=1' 'count=32' 2>/dev/null | base64)"
-              echo SENTRY_REDIS_HOST=sentry-redis
-              echo SENTRY_METRICS_SAMPLE_RATE=0.9
-              echo POSTGRES_USER=sentry
-              echo POSTGRES_DBNAME=sentry
-              echo POSTGRES_PASSWORD="$GENERATE_PASSWORD"
-              echo "#Important!!! POSTGRES_PASSWORD == SENTRY_DB_PASSWORD"
-            } >> "$INSTALLPATH"/data/sentry/sentry.env
-    fi
+      echo "Sentry env write in $INSTALLPATH/data/sentry/sentry.env"
+      echo "after first start container need run command to make migration by setting up admin user passwd"
+      echo "cd $INSTALLPATH && docker-compose -f docker-compose-infra.yml exec sentry sentry upgrade"
+      echo "---"
+      { echo SENTRY_POSTGRES_HOST=sentry-postgres
+        echo SENTRY_DB_NAME=sentry
+        echo SENTRY_DB_USER=sentry
+        echo SENTRY_DB_PASSWORD="$GENERATE_PASSWORD"
+        echo SENTRY_SECRET_KEY="$(dd 'if=/dev/random' 'bs=1' 'count=32' 2>/dev/null | base64)"
+        echo SENTRY_REDIS_HOST=sentry-redis
+        echo SENTRY_METRICS_SAMPLE_RATE=0.9
+        echo POSTGRES_USER=sentry
+        echo POSTGRES_DBNAME=sentry
+        echo POSTGRES_PASSWORD="$GENERATE_PASSWORD"
+        echo "#Important!!! POSTGRES_PASSWORD == SENTRY_DB_PASSWORD"
+      } >> "$INSTALLPATH"/data/sentry/sentry.env
+  fi
 }
 
 # @TODO
 # need check $INSTALLPATH == $COMPOSEPATH and make warning if not
 SETUPENV() {
-    GENERATED_PG_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
-    # TODO
-    # need fix created mongo container with NOC_MONGO_PASSWORD instean "noc"
-    # GENERATED_MONGO_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
-    GENERATED_MONGO_PASSWORD=noc
+  GENERATED_PG_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
+  # TODO
+  # need fix created mongo container with NOC_MONGO_PASSWORD instean "noc"
+  # GENERATED_MONGO_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)"
+  GENERATED_MONGO_PASSWORD=noc
 
-    if [ ! -f "$INSTALLPATH"/.env ]
-        then
-            echo "Writed COMPOSEPATH=$INSTALLPATH in $INSTALLPATH/.env"
-            echo "You can change the parameters OC_PG_PASSWORD\NOC_MONGO_PASSWORD if you want"
-            echo "---"
-            { echo "COMPOSEPATH=$INSTALLPATH"
-              echo "COMPOSE_HTTP_TIMEOUT=300"
-              echo "# logging driver: json-file, local, journald"
-              echo "COMPOSE_LOG_DRIVER=json-file"
-              echo "COMPOSE_LOG_MAX_SIZE=10m"
-              echo "COMPOSE_LOG_MAX_FILE=1"
-              echo "### NOC env ###"
-              echo "NOC_VERSION_TAG=$PARAM_TAG"
-              echo "# NOC_CODE_PATH '/home' for PROD or '/opt/noc' for DEV"
-              echo "NOC_CODE_PATH=$NOC_CODE_PATH"
-              echo "# Important!!! NOC_PG_PASSWORD must by similar in .data/noc/etc/noc.conf file"
-              echo "NOC_PG_PASSWORD=$GENERATED_PG_PASSWORD"
-              echo "PGPASSWORD=$GENERATED_PG_PASSWORD"
-              echo "# Important!!! NOC_MONGO_PASSWORD must by similar in .data/noc/etc/noc.conf file"
-              echo "NOC_MONGO_PASSWORD=$GENERATED_MONGO_PASSWORD"
-            } >> "$INSTALLPATH"/.env
-    fi
-
-    # make noc.conf
-   if [ ! -f "$INSTALLPATH"/data/noc/etc/noc.conf ]
+  if [ ! -f "$INSTALLPATH"/.env ]
       then
-          echo "Write $INSTALLPATH/data/noc/etc/noc.conf"
-          echo "You can change the parameters NOC_PG_PASSWORD\NOC_MONGO_PASSWORD if you want"
+          echo "Writed COMPOSEPATH=$INSTALLPATH in $INSTALLPATH/.env"
+          echo "You can change the parameters OC_PG_PASSWORD\NOC_MONGO_PASSWORD if you want"
           echo "---"
-          { echo "NOC_CONFIG=env:///NOC"
-            echo "NOC_MONGO_ADDRESSES=mongo:27017"
-            echo "NOC_PG_ADDRESSES=postgres:5432"
-            echo "NOC_CLICKHOUSE_RW_ADDRESSES=clickhouse:8123"
-            echo "NOC_CLICKHOUSE_RO_ADDRESSES=clickhouse:8123"
-            echo "NOC_CUSTOMIZATION_CUSTOM_PATH=/opt/noc_custom"
-            echo "NOC_FEATURES_CONSUL_HEALTHCHECKS=true"
-            echo "NOC_FEATURES_SERVICE_REGISTRATION=true"
-            echo "NOC_INSTALLATION_NAME=NOC-DC"
-            echo "NOC_PG_DB=noc"
-            echo "# Important!!! NOC_PG_PASSWORD must by similar in .env file"
+          { echo "COMPOSEPATH=$INSTALLPATH"
+            echo "COMPOSE_HTTP_TIMEOUT=300"
+            echo "# logging driver: json-file, local, journald"
+            echo "COMPOSE_LOG_DRIVER=json-file"
+            echo "COMPOSE_LOG_MAX_SIZE=10m"
+            echo "COMPOSE_LOG_MAX_FILE=1"
+            echo "### NOC env ###"
+            echo "NOC_VERSION_TAG=$PARAM_TAG"
+            echo "# NOC_CODE_PATH '/home' for PROD or '/opt/noc' for DEV"
+            echo "NOC_CODE_PATH=$NOC_CODE_PATH"
+            echo "# Important!!! NOC_PG_PASSWORD must by similar in .data/noc/etc/noc.conf file"
             echo "NOC_PG_PASSWORD=$GENERATED_PG_PASSWORD"
-            echo "NOC_PG_USER=noc"
-            echo "NOC_POOL=default"
-            echo "NOC_MONGO_USER=noc"
-            echo "# Important!!! NOC_MONGO_PASSWORD must by similar in .env file"
+            echo "PGPASSWORD=$GENERATED_PG_PASSWORD"
+            echo "# Important!!! NOC_MONGO_PASSWORD must by similar in .data/noc/etc/noc.conf file"
             echo "NOC_MONGO_PASSWORD=$GENERATED_MONGO_PASSWORD"
-            echo "NOC_NSQD_ADDRESSES=nsqd:4150"
-            echo "NOC_NSQD_HTTP_ADDRESSES=nsqd:4151"
-            echo "NOC_NSQLOOKUPD_ADDRESSES=nsqlookupd:4160"
-            echo "NOC_NSQLOOKUPD_HTTP_ADDRESSES=nsqlookupd:4161"
-            echo "NOC_SELFMON_ENABLE_FM=true"
-            echo "NOC_SELFMON_ENABLE_INVENTORY=true"
-            echo "NOC_SELFMON_ENABLE_TASK=true"
-            echo "NOC_FEATURES_SENTRY=false"
-            echo "# setup Sentry DSN (Deprecated) http://<ip<:9000/settings/sentry/projects/<NAMEPROJECT>/keys/"
-            echo "# NOC_SENTRY_URL=http://6ab3d0b0702d44d0acee73298a5bb40f:43d1cb7adc1946488ac9bba1d5e0dc58@sentry:9000/2"
-            echo "TZ=Europe/Moscow"
-            echo "LC_LANG=en_US.UTF-8"
-          } >> "$INSTALLPATH"/data/noc/etc/noc.conf
-   fi
+          } >> "$INSTALLPATH"/.env
+  fi
+
+  # make noc.conf
+  if [ ! -f "$INSTALLPATH"/data/noc/etc/noc.conf ]
+    then
+        echo "Write $INSTALLPATH/data/noc/etc/noc.conf"
+        echo "You can change the parameters NOC_PG_PASSWORD\NOC_MONGO_PASSWORD if you want"
+        echo "---"
+        { echo "NOC_CONFIG=env:///NOC"
+          echo "NOC_MONGO_ADDRESSES=mongo:27017"
+          echo "NOC_PG_ADDRESSES=postgres:5432"
+          echo "NOC_CLICKHOUSE_RW_ADDRESSES=clickhouse:8123"
+          echo "NOC_CLICKHOUSE_RO_ADDRESSES=clickhouse:8123"
+          echo "NOC_CUSTOMIZATION_CUSTOM_PATH=/opt/noc_custom"
+          echo "NOC_FEATURES_CONSUL_HEALTHCHECKS=true"
+          echo "NOC_FEATURES_SERVICE_REGISTRATION=true"
+          echo "NOC_INSTALLATION_NAME=NOC-DC"
+          echo "NOC_PG_DB=noc"
+          echo "# Important!!! NOC_PG_PASSWORD must by similar in .env file"
+          echo "NOC_PG_PASSWORD=$GENERATED_PG_PASSWORD"
+          echo "NOC_PG_USER=noc"
+          echo "NOC_POOL=default"
+          echo "NOC_MONGO_USER=noc"
+          echo "# Important!!! NOC_MONGO_PASSWORD must by similar in .env file"
+          echo "NOC_MONGO_PASSWORD=$GENERATED_MONGO_PASSWORD"
+          echo "NOC_NSQD_ADDRESSES=nsqd:4150"
+          echo "NOC_NSQD_HTTP_ADDRESSES=nsqd:4151"
+          echo "NOC_NSQLOOKUPD_ADDRESSES=nsqlookupd:4160"
+          echo "NOC_NSQLOOKUPD_HTTP_ADDRESSES=nsqlookupd:4161"
+          echo "NOC_SELFMON_ENABLE_FM=true"
+          echo "NOC_SELFMON_ENABLE_INVENTORY=true"
+          echo "NOC_SELFMON_ENABLE_TASK=true"
+          echo "NOC_FEATURES_SENTRY=false"
+          echo "# setup Sentry DSN (Deprecated) http://<ip<:9000/settings/sentry/projects/<NAMEPROJECT>/keys/"
+          echo "# NOC_SENTRY_URL=http://6ab3d0b0702d44d0acee73298a5bb40f:43d1cb7adc1946488ac9bba1d5e0dc58@sentry:9000/2"
+          echo "TZ=Europe/Moscow"
+          echo "LC_LANG=en_US.UTF-8"
+        } >> "$INSTALLPATH"/data/noc/etc/noc.conf
+  fi
 }
 
 while [ -n "$1" ]
@@ -212,6 +241,7 @@ if [ -n "$PARAM_P" ]
             then
                 CREATEDIR
                 SETUPENV
+                CHECKWAN
                 SETUPPROMGRAFANA
                 SETUPPROMRULES
                 SETUPSENTRY
@@ -223,6 +253,7 @@ if [ -n "$PARAM_P" ]
         elif [ "$PARAM_P" = "grafana" ]
             then
                 CREATEDIR
+                CHECKWAN
                 SETUPPROMGRAFANA
         elif [ "$PARAM_P" = "promrules" ]
             then
@@ -234,6 +265,7 @@ if [ -n "$PARAM_P" ]
         elif [ "$PARAM_P" = "env" ]
             then
                 CREATEDIR
+                CHECKWAN
                 SETUPENV
                 SETPERMISSION
         else
